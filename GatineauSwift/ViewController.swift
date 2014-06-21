7cc59380b45c4545f4a14761a3b8c240f0e3d430
Philippe Casgrain <github@casgrain.com>
2014-06-21 18:00:59 -0400

    Initial Commit

---
+++ GatineauSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GatineauSwift
+//
+//  Created by Philippe on 2014-06-21.
+//  Copyright (c) 2014 Philippe Casgrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
